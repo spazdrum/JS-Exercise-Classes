993394fb490890fb3ad5b80b4459efa5601d0910
spazdrum <spazdrum@gmail.com>
2020-02-20 21:59:24 -0500

    created constructor declaring model and milesPerGallon

--- index.js
+++ index.js
@@ -77,7 +77,12 @@ Person.prototype.toString = function () {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 }
 
 /*
